07f121822dbe48ea4254988e1b7b170091ddcb1f
Térence <terence.meunier@gmail.com>
2021-04-27 11:11:56 +0200

    Add choice of the numbers of box

--- demenagement/src/demenagement/Demenagement.java
+++ demenagement/src/demenagement/Demenagement.java
@@ -1,9 +1,16 @@
 package demenagement;
+import java.util.Scanner;
 
 public class Demenagement {
+	public static int readCartons() {
+		Scanner input = new Scanner(System.in);
+		return input.nextInt();
+	}
+	
 	public static void main(String[] args) {
 		int contenanceCamion = 9;
-		int nbCartons = 34;
+		System.out.println("Saississez le nombre de cartons (nombre entier) :");
+		int nbCartons = Demenagement.readCartons();
 		
 		while ((nbCartons - contenanceCamion) > 0) {
 			nbCartons = nbCartons - contenanceCamion;
